b966a952993a8dbaf23a9bd15b432c73f49ef5a2
PeterCiernava <63165506+PeterCiernava@users.noreply.github.com>
2022-01-31 23:41:27 +0100

    Update Main.java

--- untitled4/src/com/company/Main.java
+++ untitled4/src/com/company/Main.java
@@ -1,32 +1,49 @@
 package com.company;
-import java.util.Arrays;
-
+import java.util.Scanner;
 public class Main {
 
-    public static void main(String[] args) {
-    int[] RANDOM = new int[20];
-    int a=0;
-    int b=0;
-    int c=0;
-    for(int i=0; i < 20; i++){
-        int randomnumber = (int)(Math.random() * 50);
-        RANDOM[i] = randomnumber +1;
-        a= RANDOM[i];
-        b= c;
-        c= a+b;
-    }
-    for(int pole : RANDOM){
-        System.out.println(""+pole);
-    }
-    System.out.println("sucet:"+c);
-    float priemer = c / 20;
-    System.out.println("najvacsia hodnota:"+RANDOM[19]);
-    System.out.println("najmensia hodnota:"+RANDOM[0]);
-    System.out.println("priemerna hodnota:"+priemer);
-    Arrays.sort(RANDOM);
+    public static void main(String[] args)
+    {
+        int a, x, s, middle, last, first, y, z;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Zadajte velkost pola: ");
+        a = scan.nextInt();
+        int[] arr = new int[a];
+        System.out.print("Zadaj " +a+" cisla: ");
+        for(x=0; x<a; x++)
+            arr[x] = scan.nextInt();
+        for(x=0; x<(a-1); x++)
+        {
+            for(y=0; y<(a-y-1); y++)
+            {
+                if(arr[y]>arr[y+1])
+                {
+                    z = arr[y];
+                    arr[y] = arr[y+1];
+                    arr[y+1] = z;
+                }
+            }
+        }
+        System.out.println("\nZorad pole:");
+        for(x=0; x<a; x++)
+            System.out.print(arr[x]+ " ");
+        System.out.print("\n\nZadaj cislo: ");
+        s = scan.nextInt();
+        first = 0;
+        last = a-1;
+        middle = (first + last)/2;
+        while(first <= last) {
+            if (arr[middle] < s)
+                first = middle + 1;
+            else if (arr[middle] == s) {
+                System.out.println("\nCislo bolo najdene:" + middle + ", Pozicia cisla:" + (middle + 1));
+                break;
+            } else
+                last = middle - 1;
+
+            middle = (first + last) / 2;
+        }
+        if(first>last)
+            System.out.println("\nCislo, ktore si zadal sa nenachadza v poli");
     }
 }
-
-
-
-
